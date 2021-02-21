464ededb7025a21c5cf4f1bceefb7590017867a3
willian-m <willian.matioli@gmail.com>
2021-02-20 23:42:43 -0300

    Add a second test on the acceleration rountine

--- unit_tests/particle_system_test.cxx
+++ unit_tests/particle_system_test.cxx
@@ -80,3 +80,39 @@ BOOST_AUTO_TEST_CASE( acceleration_test ){
     delete eos;
 
 }
+
+BOOST_AUTO_TEST_CASE( acceleration_test_2 ){
+
+    EOSPolytropic* eos = new EOSPolytropic(1.,1.);
+
+    std::vector<Vec3<double>> r;
+    std::vector<Vec3<double>> v;
+    std::vector<double> m;
+    
+    //particle 1
+    r.push_back(Vec3<double>(.5,0,0));
+    v.push_back(Vec3<double>(1.,0,0));
+    m.push_back(1.);
+
+    //particle 2
+    r.push_back(Vec3<double>(-.5,0,0));
+    v.push_back(Vec3<double>(.0,0,0));
+    m.push_back(1.);
+    
+    double h = 1;
+
+    ParticleSystem test_sys(r,v,m,h,1.,1., eos);
+    
+    
+    double density_predicted = SPHMath::kernel_spline3D(Vec3<double>(0.,0.,0.), Vec3<double>(0.,0.,0.), h);
+
+    BOOST_CHECK_MESSAGE( abs(test_sys.get_acceleration(0).y) < TOL, "Non-zero acceleration on y commponent" );
+    BOOST_CHECK_MESSAGE( abs(test_sys.get_acceleration(0).z) < TOL, "Non-zero acceleration on z commponent" );
+    BOOST_CHECK_MESSAGE( abs(test_sys.get_acceleration(0).x + .5 + 6./(4.*M_PI)+1) < TOL, 
+                        "Wrong acceleration on x component. Got "<< test_sys.get_acceleration(0).x
+                        <<". It should be: " << -.5-3./(4.*M_PI) );
+    
+
+    delete eos;
+
+}
