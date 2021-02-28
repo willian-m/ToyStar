cf608dc51aa50e4a1ee5418c2270ef0500a80bf8
willian-m <willian.matioli@gmail.com>
2021-02-28 10:43:32 -0300

    Implement energy conservation checks and outputs

--- unit_tests/rk4_test.cxx
+++ unit_tests/rk4_test.cxx
@@ -5,10 +5,49 @@
 #include <vector>
 #include <stdio.h>
 #include <iomanip> 
+#include <fstream>
 
 #include "vec3.h"
 #include "eos_polytropic.h"
 #include "particle_system.h"
+
+
+
+double get_kinetic_energy(ParticleSystem* sys){
+    int npart =  sys->get_nparticles();
+    double E = 0;
+    for (int ipart=0; ipart<npart;++ipart){
+        Particle* particle = sys->get_particle(ipart);
+        Vec3<double> v = particle->get_velocity();
+        E += v.x*v.x + v.y*v.y + v.z*v.z;
+    }
+    return E;
+}
+
+double get_potential_energy(ParticleSystem* sys){
+    int npart =  sys->get_nparticles();
+    double E = 0;
+    for (int ipart=0; ipart<npart;++ipart){
+        Particle* particle = sys->get_particle(ipart);
+        Vec3<double> v = particle->get_velocity();
+        Vec3<double> r = particle->get_position();
+        E += r.x*r.x + r.y*r.y + r.z*r.z;
+    }
+    return E;
+}
+
+double get_total_energy(ParticleSystem* sys){
+    int npart =  sys->get_nparticles();
+    double E = 0;
+    for (int ipart=0; ipart<npart;++ipart){
+        Particle* particle = sys->get_particle(ipart);
+        Vec3<double> v = particle->get_velocity();
+        Vec3<double> r = particle->get_position();
+        E += v.x*v.x + v.y*v.y + v.z*v.z + r.x*r.x + r.y*r.y + r.z*r.z;
+    }
+    return E;
+}
+
 
 BOOST_AUTO_TEST_CASE( harmonic_oscillator_test ){
 
@@ -38,13 +77,49 @@ BOOST_AUTO_TEST_CASE( harmonic_oscillator_test ){
     
     //Init integrator
     IntegratorRK4 integrator(&current,&next,&buffer,dt);
+
+    std::ofstream total_E_file("Energy_HO_test.txt",std::ios::trunc);
+    std::ofstream total_K_file("Kinetic_HO_test.txt",std::ios::trunc);
+    std::ofstream total_pot_file("Potential_HO_test.txt",std::ios::trunc);
+    total_E_file << "#t\tE" << std::endl;
+    total_K_file << "#t\tE" << std::endl;
+    total_pot_file << "#t\tE" << std::endl;
+
+    double E0 = get_total_energy(&current);
+    total_E_file << "0\t" << E0 << std::endl;
+    total_K_file << "0\t" << get_kinetic_energy(&current) << std::endl;
+    total_pot_file << "0\t" << get_potential_energy(&current) << std::endl;
+
+
     
 
-    for(int istep=0; istep<nsteps;++istep){
-        integrator.do_step();
-        integrator.update_system();
-    }
 
+    for(int istep=0; istep<nsteps;++istep){
+        double E1 = get_total_energy(&current);
+        integrator.do_step();
+        BOOST_CHECK_MESSAGE( abs(E1 - E0) < TOL*100, "Energy changed more than tolerable");
+        total_E_file << istep*dt + dt <<"\t"<< E1 << std::endl;
+        total_K_file << istep*dt + dt <<"\t" << get_kinetic_energy(&current) << std::endl;
+        total_pot_file << istep*dt + dt <<"\t" << get_potential_energy(&current) << std::endl;
+        if ((istep == nsteps/4) || (istep == 3*nsteps/4)){
+            BOOST_CHECK_MESSAGE( abs(current.get_particle(0)->get_x() ) < TOL*100,
+                        "Particle at position: "<<current.get_particle(0)->get_x()<<
+                        ". Expected at 0.");
+        } else if (istep == nsteps/2){
+            BOOST_CHECK_MESSAGE( abs(current.get_particle(0)->get_x() + 1 ) < TOL*100,
+                        "Particle at position: "<<current.get_particle(0)->get_x()<<
+                        ". Expected at -1.");
+            BOOST_CHECK_MESSAGE( abs(current.get_particle(0)->get_vx()) < TOL*100,
+                        "Particle with velocity: "<<current.get_particle(0)->get_x()<<
+                        ". Expected at 0.");
+        }
+    }
+    total_E_file << std::flush;
+    total_E_file.close();
+    total_pot_file << std::flush;
+    total_pot_file.close();
+    total_K_file << std::flush;
+    total_K_file.close();
     std::cout << "Position deviation from expected: "<< std::setprecision(15) << abs(current.get_particle(0)->get_x() - 1.) << std::endl;
     BOOST_CHECK_MESSAGE( abs(current.get_particle(0)->get_x() - 1.) < TOL*100,
                         "Particle at position: "<<current.get_particle(0)->get_x()<<
@@ -90,7 +165,6 @@ BOOST_AUTO_TEST_CASE( damped_harmonic_oscillator ){
     
     for(int istep=0; istep<nsteps;++istep){
         integrator.do_step();
-        integrator.update_system();
     }
     std::cout << std::endl;
     std::cout << "Position deviation from expected: "<< std::setprecision(15) << abs(current.get_particle(0)->get_x() ) << std::endl;
@@ -105,3 +179,64 @@ BOOST_AUTO_TEST_CASE( damped_harmonic_oscillator ){
 
     
 }
+
+/*BOOST_AUTO_TEST_CASE( update_system_test ){
+    //setup particle system
+    EOSPolytropic* eos = new EOSPolytropic(0.,1.);
+
+    int const n_points = 10; //Test will generate this many random points.
+                             //The distance between them should always be positive
+    std::vector<Vec3<double>> pos1, vel1;
+    std::vector<Vec3<double>> pos2, vel2;
+    std::vector<double> mass1, mass2;
+    Vec3<double> pos, vel;
+
+
+    std::srand(8270509);
+
+    for (int ipoint=0; ipoint<n_points;++ipoint){
+        pos.x = (double)(rand()%100);
+        pos.y = (double)(rand()%100);
+        pos.z = (double)(rand()%100);
+        pos1.push_back(pos);
+        vel.x = (double)(rand()%100);
+        vel.y = (double)(rand()%100);
+        vel.z = (double)(rand()%100);
+        vel1.push_back(vel);
+        double mass = (double)(rand()%100);
+        mass1.push_back(mass);
+        pos.x = (double)(rand()%100);
+        pos.y = (double)(rand()%100);
+        pos.z = (double)(rand()%100);
+        pos2.push_back(pos);
+        vel.x = (double)(rand()%100);
+        vel.y = (double)(rand()%100);
+        vel.z = (double)(rand()%100);
+        vel2.push_back(vel);
+        mass = (double)(rand()%100);
+        mass2.push_back(mass);
+    }
+
+    ParticleSystem current(pos1,vel1,mass1,.05,1.,0.,eos);
+    ParticleSystem next(pos2,vel2,mass2,.05,1.,0.,eos);
+    ParticleSystem buffer(pos2,vel2,mass2,.05,1.,0.,eos);
+
+    std::cout << &current <<"," <<&next<<std::endl;
+    
+
+    IntegratorRK4 integrator(&current,&next,&buffer,1.);
+    integrator.update_system();
+    std::cout << &current <<"," <<&next<<std::endl;
+    
+    //Check if we swapped the systems
+    for (int ipart=0;ipart<n_points;++ipart){
+        Particle* next_part = next.get_particle(ipart);
+        BOOST_CHECK_MESSAGE( next_part->get_x() == pos1[ipart].x,
+            "Wrong particle position in x direction. Expected "<<pos1[ipart].x<<". Received: "<<next_part->get_x());
+        BOOST_CHECK_MESSAGE( next_part->get_y() == pos1[ipart].y,
+            "Wrong particle position in y direction. Expected "<<pos1[ipart].y<<". Received: "<<next_part->get_y());
+        BOOST_CHECK_MESSAGE( next_part->get_z() == pos1[ipart].z,
+            "Wrong particle position in z direction. Expected "<<pos1[ipart].z<<". Received: "<<next_part->get_z());
+    }
+    
+}*/
