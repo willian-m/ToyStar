62b950214df7a28b692fc87846310f7af90c5bb6
willian-m <willian.matioli@gmail.com>
2021-03-19 15:28:03 -0300

    Add test to two subsequent calls of update_acceleration

--- unit_tests/particle_system_test.cxx
+++ unit_tests/particle_system_test.cxx
@@ -64,6 +64,61 @@ BOOST_AUTO_TEST_CASE( neighbour_test ){
 
 }
 
+BOOST_AUTO_TEST_CASE( neighbour_test_subsequent_calls ){
+
+    //A) Create a 3D system with 400 particles placed randomly
+    std::vector<Vec3> r;
+    std::vector<Vec3> v;
+    std::vector<double> m;
+
+    EOSPolytropic* eos = new EOSPolytropic(1.,1.);
+
+
+    std::srand(8270509);
+
+    for (int ipoint=0; ipoint<400;++ipoint){
+        double x = (double)((rand()-RAND_MAX/2))/RAND_MAX;
+        double y = (double)((rand()-RAND_MAX/2))/RAND_MAX;
+        double z = (double)((rand()-RAND_MAX/2))/RAND_MAX;
+
+        r.push_back(Vec3(x,y,z));
+        v.push_back(Vec3(0,0,0));
+        m.push_back(1.);
+    }
+    double h = 0.05;
+    ParticleSystem<Vec3> sys(r, v, m, h, 0.,1.,eos);
+
+    sys.update_acceleration(); //Triggers algorithm of filling particle neighbors
+    sys.update_acceleration(); //Second call
+
+    
+    for(int ipart=0; ipart < 400; ++ipart){
+        Particle<Vec3>* part = sys.get_particle(ipart);
+        std::vector<Particle<Vec3>*> neigh_part;
+        //Loop over all particles, finding the ones that are in the neighborhood of ipart
+        for(int jpart=0; jpart < 400; ++jpart){
+            Particle<Vec3>* part_candidate = sys.get_particle(jpart);
+            double d = SPHMath::distance(part->get_position(),part_candidate->get_position());
+            if ( d < 2*h && d > 1.e-14 ) neigh_part.push_back(part_candidate);
+        }
+        //Now we test if all neighbour particles were included in the neighbour list of ipart
+        int num_neighbors = part->get_num_neighbors();
+        BOOST_CHECK_MESSAGE( num_neighbors == neigh_part.size(),
+                            "Particle neighbour list has different size of actual neighbors. "
+                          <<"Number of neighbors in list: " << num_neighbors <<". Actual number of neighbors: " <<neigh_part.size());
+        for (int ipart_neigh=0; ipart_neigh << num_neighbors; ++ipart_neigh){
+            Particle<Vec3>* part_neigh_candidate = part->get_neighbor(ipart_neigh);
+            bool particle_found = false;
+            for( Particle<Vec3>* actual_part_neigh : neigh_part){
+                if (actual_part_neigh == part_neigh_candidate) particle_found = true;
+            }
+            BOOST_CHECK_MESSAGE(particle_found,"Missing particle in the neighborhood list found.");
+        }
+         
+    }
+
+}
+
 BOOST_AUTO_TEST_CASE( density_test ){
 
     EOSPolytropic* eos = new EOSPolytropic(1.,1.);
